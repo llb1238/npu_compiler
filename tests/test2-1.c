f44963b8135090cddc2907907e28f1a4e2cdef07
Compressedball <2488965730@qq.com>
2025-04-28 19:38:43 +0800

    test

--- tests/test2-1.c
+++ tests/test2-1.c
@@ -1,22 +1,8 @@
 int main()
 {
-	int a, c;
-    int b;
-    a = 3;
-    b = 2;
-    {
-        int c;
-        c = a + b;
-        b = c;
-	}
-
-	{
-        int a, b;
-        a = 4;
-        b = 5;
-        c = a + b;
-
-	}
-
-	return a + b + c;
+    float a;
+    float b;
+    a=2.1;
+    b=2.2+3.3;
+    return 0;
 }
