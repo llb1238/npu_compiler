ca4512ca952f71e8d28824c89f224561f1729522
Wang Chongyu <147987887+sword050323@users.noreply.github.com>
2025-04-28 13:20:41 +0800

    Update test1-1.c

--- tests/test1-1.c
+++ tests/test1-1.c
@@ -1,10 +1,32 @@
-int main()
-{
-    int a;
-    int b;
-    int c,d;
-    a = 2;
-    b = 3;
+const int C = 10;
+const int D[2] = {1, 2};
 
-    return a + b + 2;
+int foo() {
+    return C;
+}
+
+int bar(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int a;
+    int arr[5];
+    a = 1 + 2;
+
+    if (a > 2) {
+        a = bar(1, 2);
+    } else {
+        a = foo();
+    }
+
+    while (a < 10) {
+        a = a + 1;
+        if (a == 5) {
+            break;
+        }
+        continue;
+    }
+
+    return arr[0];
 }
