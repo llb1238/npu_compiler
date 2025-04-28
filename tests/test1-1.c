6558b77a1f41ef0a2a81ad47409c61686a966e1c
sword050323 <2385816795@qq.com>
2025-04-28 19:16:57 +0800

    ceshi

--- tests/test1-1.c
+++ tests/test1-1.c
@@ -1,32 +1,4 @@
-const int C = 10;
-const int D[2] = {1, 2};
-
-int foo() {
-    return C;
-}
-
-int bar(int x, int y) {
-    return x + y;
-}
-
+float C = 10;
 int main() {
-    int a;
-    int arr[5];
-    a = 1 + 2;
-
-    if (a > 2) {
-        a = bar(1, 2);
-    } else {
-        a = foo();
-    }
-
-    while (a < 10) {
-        a = a + 1;
-        if (a == 5) {
-            break;
-        }
-        continue;
-    }
-
-    return arr[0];
+    return C;
 }
